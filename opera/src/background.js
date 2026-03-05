782fe4895c8cf3c4904e08a6fcc9f95a11665a68
Ryan Doiron <rdoiron@gmail.com>
2026-03-05 14:09:46 -0500

    feat: upgrade Firefox & Opera to Manifest V3, add Edge browser support
    - Firefox: migrate to MV3 with action API (enable/disable pattern, no declarativeContent)
    - Opera: migrate to MV3 with declarativeContent + service_worker (same as Chrome)
    - Edge: add new build target mirroring Chrome's MV3 setup
    - Add webpack configs for all browsers and Edge build/package scripts

--- opera/src/background.js
+++ opera/src/background.js
@@ -1301,37 +1301,49 @@ var luminateEdit = {
     }
   }
 };
-
 /*
- * Luminate Online Page Editor - Opera Next
+ * Luminate Online Page Editor - Opera
  * luminateEdit-operaNext.js
  * Version: 1.12 (15-NOV-2017)
+ * Updated for Manifest V3 compatibility
  */
+
+/* set up declarativeContent rules on install to show action for potential Luminate Online pages */
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({ pageUrl: { urlContains: '/site/' } }),
+        new chrome.declarativeContent.PageStateMatcher({ pageUrl: { urlContains: '/images/content/pagebuilder/' } })
+      ],
+      actions: [new chrome.declarativeContent.ShowAction()]
+    }]);
+  });
+});
 
 luminateEdit.operaNext = {
   /* checks the current URL for known front-end servlet names, as defined in luminateEdit.servlets */
   checkForLuminateOnlineUrl: function(tabId, changeInfo, tab) {
-    /* set the tabUrl and show the button as soon as the tab starts loading */
     if(changeInfo.status == 'loading') {
       luminateEdit.tabUrl = tab.url.replace('view-source:', '');
-      
+
       var currentServlet = luminateEdit.getCurrentServlet();
-      if(currentServlet != null && luminateEdit.servlets[currentServlet] && 
-         luminateEdit.servlets[currentServlet].getUrl() != null) {
-        chrome.pageAction.show(tabId);
+      /* hide the action if URL broadly matches but is not a known servlet */
+      if(currentServlet == null || !luminateEdit.servlets[currentServlet] || luminateEdit.servlets[currentServlet].getUrl() == null) {
+        chrome.action.hide(tabId);
       }
     }
-  }, 
-  
+  },
+
   /* go to the admin URL when the edit icon is clicked */
   goToEditUrl: function() {
     /* update the tab URL to ensure it is up-to-date at the time the icon is clicked */
     chrome.tabs.query({
-      active: true, 
+      active: true,
       windowId: chrome.windows.WINDOW_ID_CURRENT
     }, function(allTabs) {
       luminateEdit.tabUrl = allTabs[0].url.replace('view-source:', '');
-      
+
       var currentServlet = luminateEdit.getCurrentServlet();
       if(luminateEdit.tabUrl != null && currentServlet != null) {
         var adminBaseUrl = luminateEdit.tabUrl.split('/site/')[0];
@@ -1339,7 +1351,7 @@ luminateEdit.operaNext = {
         if(luminateEdit.tabUrl.indexOf('/images/content/pagebuilder/') != -1) {
           adminBaseUrl = luminateEdit.tabUrl.split('/images/')[0];
         }
-        
+
         chrome.tabs.create({
           url: adminBaseUrl + '/site/' + luminateEdit.servlets[currentServlet].getUrl()
         });
@@ -1350,4 +1362,4 @@ luminateEdit.operaNext = {
 
 /* bind listeners */
 chrome.tabs.onUpdated.addListener(luminateEdit.operaNext.checkForLuminateOnlineUrl);
-chrome.pageAction.onClicked.addListener(luminateEdit.operaNext.goToEditUrl);
+chrome.action.onClicked.addListener(luminateEdit.operaNext.goToEditUrl);
